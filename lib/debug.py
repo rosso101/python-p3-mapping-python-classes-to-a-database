a4478075abce020d50686810a627c3e3ddf1c629
rosso101 <victorprivv101@gmail.com>
2023-12-05 12:33:30 +0300

    completed assignmentz

--- lib/debug.py
+++ lib/debug.py
@@ -6,3 +6,7 @@ from song import Song
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+    
+    Song = Song.create("Hello", "25")
+    print("Song Name:", song.name)
+    print("Song Album:", song.album)
